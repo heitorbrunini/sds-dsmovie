a928c71680d23e188cf41f9895ebcc47faae3742
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2022-02-20 01:25:50 -0300

    pagination layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -9,34 +9,35 @@ function Form() {
         score: 4.5
     }
     return (
-        <div className="dsmovie-form-container">
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3> {movie.title}</h3>
-                <form className="form-group dsmovie-form-group">
-                    <div className="dsmovie-form">
-                        <label htmlFor="email">Informe seu Email</label>
-                        <input type="email" className="form-control"></input>
-                    </div>
-                    <div className="form-group dsmovie-form-group">
-                        <label htmlFor="score">Informe sua Avaliação</label>
-                        <select className="form-control" id="score">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
+
+            <div className="dsmovie-form-container">
+                <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+                <div className="dsmovie-card-bottom-container">
+                    <h3> {movie.title}</h3>
+                    <form className="form-group dsmovie-form-group">
+                        <div className="dsmovie-form">
+                            <label htmlFor="email">Informe seu Email</label>
+                            <input type="email" className="form-control"></input>
+                        </div>
+                        <div className="form-group dsmovie-form-group">
+                            <label htmlFor="score">Informe sua Avaliação</label>
+                            <select className="form-control" id="score">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                    </form>
                     <div className="dsmovie-form-btn-container">
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
-                </form>
-                <div className="dsmovie-form-btn-container">
-                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                    <div className="dsmovie-form-btn-container">
+                        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                    </div>
                 </div>
             </div>
-        </div>
     );
 }
 
