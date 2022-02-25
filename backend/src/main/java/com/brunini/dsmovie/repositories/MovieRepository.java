89b3e24a4037a945fe5b7f3541c631aeb09118d4
heitorbrunini <heitor.brunini@gmail.com>
2022-02-25 15:04:50 -0300

    Find movies

---
+++ backend/src/main/java/com/brunini/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.brunini.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brunini.dsmovie.entities.Movie;
+
+public interface MovieRepository extends   JpaRepository<Movie, Long>{
+
+}
